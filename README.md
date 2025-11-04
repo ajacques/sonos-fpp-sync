ed1656ef861dbe46af2c6b203402a5ab59649080
Adam Jacques <ajacques@users.noreply.github.com>
2025-11-03 21:48:00 -0800

    More work. serve from local files

---
+++ README.md
@@ -0,0 +1,10 @@
+show_plan.json:
+
+```json
+[
+    {
+        "stream_uri": "https://blah",
+        "fseq_file": "testfile.fseq"
+    }
+]
+```
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from collections import namedtuple
 from pprint import pprint
 from soco import events_twisted
-from twisted.internet import reactor, udp
+from twisted.internet import reactor
+from twisted.web.server import Site
+from twisted.web import static
+from twisted.web.resource import Resource
 from twisted.internet.protocol import DatagramProtocol
 import fseq
-import glob
-import logging
 import signal
 import socket
 import soco
 import struct
 import datetime
 import sys
-import pathlib
+import json
 soco.config.EVENTS_MODULE = events_twisted
 #logging.basicConfig(level=logging.DEBUG)
 
@@ -21,20 +22,21 @@ device = soco.discovery.any_soco()
 
 device = device.group.coordinator
 
-SeqDetails = namedtuple('SequenceInfo', ['num_frames'])
+SeqDetails = namedtuple('SequenceInfo', ['num_frames', 'fseq_file', 'stream_path'])
 
-fseq_table: dict[str, int] = {}
+def load_show_plan() -> list[SeqDetails]:
+    result = []
+    with open('show_plan.json') as f:
+        show_plan = json.load(f)
+        for seq in show_plan:
+            fseq_file_name = seq['fseq_file']
+            with open(fseq_file_name, 'rb') as fseq_file_fd:
+                fseq_file = fseq.parse(fseq_file_fd)
+                num_frames = fseq_file.number_of_frames
+            result.append(SeqDetails(num_frames, fseq_file_name, seq['stream_path']))
+    return result
 
-
-for fseq_file_name in glob.glob('../../xlights-test/**.fseq'):
-    with open(fseq_file_name, 'rb') as f:
-        fseq_file = fseq.parse(f)
-        fseq_table[fseq_file_name] = fseq_file.number_of_frames
-
-print(fseq_table)
-
-print(device)
-track_info = device.get_current_track_info()
+fseq_table = load_show_plan()
 
 MULTICAST_ADDRESS = '239.70.80.80'
 PORT = 32320
@@ -53,13 +55,16 @@ signal.signal(signal.SIGINT, signal_handler)
 
 print("Listening for FPP multisync packets on", MULTICAST_ADDRESS, ":", PORT)
 
-def get_lan_addr():
+def get_lan_addr_str() -> str:
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("192.168.2.1", 80))
-        return socket.inet_aton(s.getsockname()[0])
+        return s.getsockname()[0]
     finally:
         s.close()
+
+def get_lan_addr():
+        return socket.inet_aton(get_lan_addr_str())
 
 def encode_str(string: str, target_length: int) -> str:
     return string.ljust(target_length, '\0').encode('ascii')
@@ -77,10 +82,7 @@ def parse_time(time_str):
     h, m, s = map(int, time_str.split(':'))
     return h * 3600 + m * 60 + s
 
-seconds_elapsed = parse_time(track_info['position'])
-total_seconds = parse_time(track_info['duration'])
-
-def encode_sync_packet(sequence_name: str):
+def encode_sync_packet(sequence_name: str, fseq_file: SeqDetails, seconds_elapsed, total_seconds):
     return struct.pack(
         f'<4sBHBBIf{len(sequence_name)}sB', 
         'FPPD'.encode('ascii'), 
@@ -116,7 +118,7 @@ def encode_hello_packet():
 class MulticastListener(DatagramProtocol):
     def startProtocol(self):
         # Join the multicast group
-        self.transport.joinGroup(MULTICAST_ADDRESS)  # Replace with your multicast address
+        self.transport.joinGroup(MULTICAST_ADDRESS)
 
         # Introduce ourselves to the FPP MultiSync swarm
         self.transport.write(encode_hello_packet(), (MULTICAST_ADDRESS, PORT))
@@ -127,13 +129,17 @@ class MulticastListener(DatagramProtocol):
             return
         print(f"Received packet from {address}")
 
-
 def process_sonos_packet(event: soco.events_base.Event):
     pprint(event.variables)
     cur_time = datetime.datetime.now()
+    #elapsed = parse_time(event.variables['current_track_position'])
     duration = parse_time(event.variables['current_track_duration'])
 
+    #start_time = cur_time - datetime.timedelta(seconds=elapsed)
+
     event.variables
+    #print(start_time, duration)
+    #encode_sync_packet('romeo.fseq', fseq_table[0], elapsed, duration)
     #print("Packet")
     #pprint(event)
 
@@ -142,9 +148,7 @@ def main():
     device = soco.discovery.any_soco()
 
     device = device.group.coordinator
-    # Subscribe to ZGT evI ents
     sub = device.avTransport.subscribe(auto_renew=True).subscription
-    # print out the events as they arise
     sub.callback = process_sonos_packet
 
     def before_shutdown():
@@ -153,7 +157,14 @@ def main():
 
     reactor.addSystemEventTrigger('before', 'shutdown', before_shutdown)
 
-if __name__=='__main__':
+    device.clear_queue()
+    local_ip = get_lan_addr_str()
+    print(local_ip)
+    for show in fseq_table:
+        device.add_uri_to_queue(f"http://{local_ip}:8080/{show.stream_path}")
+
+if __name__ == '__main__':
     reactor.listenMulticast(PORT, MulticastListener(), listenMultiple=True)
+    reactor.listenTCP(8080, Site(static.File("./songs")))
     reactor.callWhenRunning(main)
     reactor.run()
